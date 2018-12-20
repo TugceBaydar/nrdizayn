ce7f54606db91bc83b3b2f4a8bde3b49472c0b9f
TugceBaydar <tugcebaydar06@gmail.com>
2018-12-20 13:04:37 +0300

    commit 12

--- js/main.js
+++ js/main.js
@@ -34,22 +34,42 @@ document.querySelector(".dropdown").addEventListener("mouseleave", function(e)
 
 
 // Get the modal
-var modal = document.getElementById('myModal');
+// var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+// var img = document.getElementById('myImg');
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+// span.onclick = function() {
+  // modal.style.display = "none";
+// }
+
+
+function modal(id) {
+  var modal = document.getElementById('myModal')
+
+  var img=document.getElementById('id');
+  var modalImg = document.getElementById('img01');
+  var captionText = document.getElementById("caption");
+
+  img.onclick=function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innetHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+ span.onclick =function(){
   modal.style.display = "none";
 }
+};
